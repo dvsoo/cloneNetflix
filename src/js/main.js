fdc70947ebc0758be970f1c887477a9475eb3b7d
dvsoo <qpzm2480@naver.com>
2018-10-14 20:57:18 +0900

    header and video js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,32 @@
+const header = document.querySelector('.header'),
+    SCROLL_ACTIVE = "header-bg";
+const video = document.querySelector('.js-video'),
+    VIDEO_MUTE = `<i class="fas fa-volume-mute"></i>`,
+    jsMuteBtn = document.querySelector('.js-mute');
+
+video.autoplay = true;
 
 const handleHeader = () => {
-    const header = document.querySelector('.header'),
-        SCROLL_ACTIVE = "header-bg";
-    
+
     if(window.scrollY > 100){
         header.classList.add(SCROLL_ACTIVE);
+        video.pause();
     }else{
         header.classList.remove(SCROLL_ACTIVE);
+        video.play();
+    }
+};
+
+const handleVideoMute = () => {
+    const toggleClass = jsMuteBtn.classList;
+    toggleClass.toggle
+    if(video.muted){
+        console.log('hi');
+    }else{
+        console.log('no');
     }
 };
 
 window.addEventListener('scroll', handleHeader);
+jsMuteBtn.addEventListener('click', handleVideoMute);
+
